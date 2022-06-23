34b0ce63e684cd3984e78e91e027f49e90228227
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-06-23 23:20:27 +0300

    add AuthService, SharingModule

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +13,7 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -21,5 +24,14 @@ export class LoginPageComponent implements OnInit {
 
   submit() {
     if (this.form.invalid) return;
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+    this.auth.login(user).subscribe(()=>{
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }
