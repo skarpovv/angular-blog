aeee2b30ebf398a56f5faf4f10b6cc32195e36ba
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-06-23 23:56:01 +0300

    add FireBase

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {User} from "../../../shared/interfaces";
-import {Observable} from "rxjs";
+import {FbAuthResponse, User} from "../../../shared/interfaces";
+import {Observable, tap} from "rxjs";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,8 @@ export class AuthService {
   }
 
   login(user: User): Observable<any>{
-    return this.http.post('',user)
+    return this.http.post<FbAuthResponse>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,user)
+      .pipe(tap(this.setToken))
   }
 
   logout(){
@@ -24,7 +26,7 @@ export class AuthService {
     return !!this.token
   }
 
-  private setToken(){
-
+  private setToken(response: FbAuthResponse){
+    console.log(response);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyDNZ_jfD5rfTUGy44rs16XBhCUhRjDyhTg',
 };
 
 /*
